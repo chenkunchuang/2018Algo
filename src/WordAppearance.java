7b8a4b8bd57dde2767f6b3ee630d4d7cef0732a8
chenkun.chuang <chenkun.chuang@gmail.com>
2018-02-08 16:16:20 -0800

    algorithm to calculate word appearance

--- src/WordAppearance.java
+++ src/WordAppearance.java
@@ -1,2 +1,35 @@
+import java.util.*;
+
 public class WordAppearance {
+
+    /*
+    *  This method calculate the appearance of words using google: map and reduce
+     */
+    private void gMapRedeuce(String[] input, HashMap<String, Integer> output) {
+
+         for(String s:input) {
+
+             String[] words = s.split(" ");
+             for(String word:words) {
+
+                 output.put(word, output.getOrDefault(word, 0)+1);
+
+             }
+
+         }
+         for(Map.Entry<String, Integer> p : output.entrySet()) {
+             System.out.println("word: " + p.getKey() + " appearance: " + p.getValue());
+         }
+    }
+
+
+    public static void main(String[] args) {
+
+        String[] input = {"Deer Bear River", "Car Car River", "Deer Car Bear"};
+
+        WordAppearance solution = new WordAppearance();
+
+        solution.gMapRedeuce(input, new HashMap<>());
+
+    }
 }
