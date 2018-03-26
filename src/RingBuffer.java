afb84db7ed766c150573247c00b43f020cc9eb0b
chenkun.chuang <chenkun.chuang@gmail.com>
2018-03-26 09:32:28 -0700

    Add logic comments to determine full/empty

--- src/RingBuffer.java
+++ src/RingBuffer.java
@@ -2,6 +2,9 @@
 *  This class is implementing ring buffer which is non-blocking queue
 *  Put to the tail, take from head so for consumer/producer will not need to
 *   be blocked when either one is touch queue
+*   NOTE: need to keep the last location as empty so it can determine
+*   Full: (tail+1)%MAX_SIZE==head
+*   Empty: tail==head
  */
 
 public class RingBuffer {
