98f04660a7ae02fceb9d5fed0051ad4fbd40fd9c
chenkun <chenkun.chuang@gmail.com>
2018-09-18 21:38:43 -0700

    remove extra letters and use dfs to see if by removing extra letters, only add one or two letter to form a new word in dictionary or not.

--- src/ExpressiveWord.java
+++ src/ExpressiveWord.java
@@ -7,6 +7,7 @@ for example, heeeeellooooo --> e(1,5) o(8,12) any characters repeated at least 3
 public class ExpressiveWord {
     private HashSet<String> dict;
     public ExpressiveWord(String[] words) {
+        dict = new HashSet<>();
         for(String word: words)
             dict.add(word);
     }
@@ -37,30 +38,55 @@ public class ExpressiveWord {
         return ans;
     }
 
-    public String matchWord(String word) {
+    public List<String> matchWord(String word) {
         List<int[]> chars = find(word);
-        List<String> candidate;
-        StringBuilder sb = new StringBuilder();
-        int i=0;
-        for(int[] pair: chars) {
-            int start = pair[1];
-            int end = pair[2];
-            sb.append(word.substring(i, start));
-            for(int i=0; i<2;  i++) {
+        List<String> candidates = new ArrayList<>();
+        List<String> ans = new ArrayList<>();
+//        StringBuilder sb = new StringBuilder();
+        //只有贅字的時候才需要dfs
+//        if(chars.size()>0)
+        dfs(chars, 0, candidates, 0, word, "");
+        System.out.println("possible candidates:"+candidates);
 
+        for(String s: candidates) {
+            if(dict.contains(s))
+                ans.add(s);
+        }
+        return ans;
+    }
+
+    public void dfs(List<int[]> indices, int pairIdx, List<String> candidates, int idx, String word, String new_word) {
+        if(idx==word.length()) {
+            candidates.add(new_word);
+            return;
+        }
+        if(pairIdx<indices.size()) {
+            int[] pair = indices.get(pairIdx);
+            while(idx<word.length()&&idx<pair[1]) {
+                new_word+=word.charAt(idx++);
             }
+            dfs(indices, pairIdx+1, candidates, pair[2]+1, word, new_word+(char)pair[0]);
+            dfs(indices, pairIdx+1, candidates, pair[2]+1, word, new_word+(char)pair[0]+(char)pair[0]);
+        }
+        else {
+            while(idx<word.length()) {
+                new_word+=word.charAt(idx++);
+            }
+            dfs(indices, pairIdx, candidates, idx, word, new_word);
         }
     }
 
     public static void main(String[] args) {
-        ExpressiveWord sol = new ExpressiveWord();
-        String[] words = {"hello", "hi", "cat", "world"};
-        sol.matchWord(words);
+        String[] words = {"hello", "hi", "cat", "world","hee","hellotg"};
+        ExpressiveWord sol = new ExpressiveWord(words);
+        String[] extensiveWords = {"heeeellooooo","hiiiiiiiiiii","hee","heeellooootttggg","cccaaatttt"};
+        for(String s: extensiveWords)
+            System.out.println("extensive word:"+s+" and word in dictionary:"+sol.matchWord(s));
 
-        sol.find("heeeellooooo");
-        sol.find("hiiiiiiiiiii");
-        sol.find("");
-        sol.find("hee");
-        sol.find("heeellooootttggg");
+//        sol.find("heeeellooooo");
+//        sol.find("hiiiiiiiiiii");
+//        sol.find("");
+//        sol.find("hee");
+//        sol.find("heeellooootttggg");
     }
 }
